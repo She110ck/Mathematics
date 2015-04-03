8fb62500a0edc89bedb373be0070d2dd3a0cc515
She110ck <hasan.hasanov1@yahoo.com>
2015-04-03 07:34:37 +0500

    koshi

---
+++ Koshi Meselesi/MainForm/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace MainForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataSolution = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataPikar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataAEyler = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataQEyler = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataRunqe = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataAdams = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(884, 94);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(544, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(166, 68);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Adams üsulu üçün";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(78, 17);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Aşkar Eyler";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(18, 45);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(97, 17);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Runqe-Kutta(4)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox5.Location = new System.Drawing.Point(764, 51);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(56, 20);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.Control;
+            this.label7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label7.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label7.Location = new System.Drawing.Point(393, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 23);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Sil";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            this.label7.MouseLeave += new System.EventHandler(this.label7_MouseLeave);
+            this.label7.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label7_MouseMove);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(760, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 22);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "N=2";
+            this.label6.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.label6_MouseDoubleClick);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(253, 51);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(212, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "U(0)=";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(253, 25);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(226, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "n=";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "b=";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "a=";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(393, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hesabla";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 94);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(884, 562);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataX,
+            this.dataSolution,
+            this.dataPikar,
+            this.dataAEyler,
+            this.dataQEyler,
+            this.dataRunqe,
+            this.dataAdams});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(884, 562);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataX
+            // 
+            this.dataX.HeaderText = "X";
+            this.dataX.Name = "dataX";
+            this.dataX.ReadOnly = true;
+            // 
+            // dataSolution
+            // 
+            this.dataSolution.HeaderText = "Dəqiq həll";
+            this.dataSolution.Name = "dataSolution";
+            this.dataSolution.ReadOnly = true;
+            // 
+            // dataPikar
+            // 
+            this.dataPikar.HeaderText = "Pikar (y3)";
+            this.dataPikar.Name = "dataPikar";
+            this.dataPikar.ReadOnly = true;
+            // 
+            // dataAEyler
+            // 
+            this.dataAEyler.HeaderText = "Aşkar Eyler";
+            this.dataAEyler.Name = "dataAEyler";
+            this.dataAEyler.ReadOnly = true;
+            // 
+            // dataQEyler
+            // 
+            this.dataQEyler.HeaderText = "Q/Aşkar Eyler";
+            this.dataQEyler.Name = "dataQEyler";
+            this.dataQEyler.ReadOnly = true;
+            // 
+            // dataRunqe
+            // 
+            this.dataRunqe.HeaderText = "Runqe-Kutta(4)";
+            this.dataRunqe.Name = "dataRunqe";
+            this.dataRunqe.ReadOnly = true;
+            // 
+            // dataAdams
+            // 
+            this.dataAdams.HeaderText = "Adams";
+            this.dataAdams.Name = "dataAdams";
+            this.dataAdams.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 656);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Ededi Usullar";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataX;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataSolution;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataPikar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataAEyler;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataQEyler;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataRunqe;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataAdams;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Koshi Meselesi/MainForm/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainForm
+{
+    public partial class Form1 : Form
+    {
+        public static int n=10;
+        public const int SAY = 6;
+        public static int NUM=2;
+        public double a, b, u0, h;
+        
+        public double[] x = null; //new double[n + 1];
+        public double[,] y = null; //new double[SAY, n + 1];
+        public double[,] k = null;//new double[4, n + 1];
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void initValues() {
+          //  a = 0;
+           // b = 1;
+           // n = 10;
+          //  u0 = 0;
+            n = int.Parse(textBox3.Text);
+            x = new double[n + 1];
+            y = new double[SAY, n + 1];
+            k = new double[4, n + 1];
+            a = double.Parse(textBox1.Text);
+            b = double.Parse(textBox2.Text);
+            
+            u0 = double.Parse(textBox4.Text);
+            h = (double)(b - a) / n;
+            for (int i = 0; i < n+1; i++) {
+                x[i] = a + i * h;
+            }
+            for (int i = 0; i < SAY; i++)
+            {
+                y[i, 0] = u0;
+            }
+        }
+
+        public void deqiqHell()
+        {
+            double c = 3;
+            //(u0  + NUM) / Math.Exp(a);
+            for (int i = 0; i < n + 1; i++)
+            {
+                y[0, i] = c * Math.Exp(x[i]) - 2 * x[i] - NUM;
+            }
+        }
+
+        public void pikar()
+        {
+            for (int i = 0; i < n + 1; i++)
+            {
+                y[1, i] = (Math.Pow(x[i], 2) / fkt(2) + Math.Pow(x[i], 3) / fkt(3) + Math.Pow(x[i], 4) / fkt(4)) * NUM + u0 + u0 * Math.Pow(x[i], 3) / fkt(3);
+            
+            }
+
+        }
+
+        public void ashkarEyler() {
+
+            for (int i = 0; i < n; i++)
+            {
+                y[2, i + 1] = y[2, i] + h * func(x[i], y[2, i]);
+            }
+        }
+        public void qeyriAshkarEyler() {
+
+            for (int i = 0; i < n; i++)
+            {
+                double yd = y[3, i] + h * func(x[i], y[3, i]);
+                y[3, i + 1] = y[3, i] + (h/2)*( func(x[i], y[3, i])+func(x[i+1],yd));
+            }
+        }
+        public void adams() {
+            if (radioButton1.Checked)
+            {
+                for (int i = 0; i < 4; i++) y[5, i] = y[2, i];
+            }
+            else {
+                for (int i = 0; i < 4; i++) y[5, i] = y[4, i];
+            }
+            for (int i = 3; i < n; i++)
+            { 
+            y[5,i+1]= y[5,i]+(h/24)*(55*func(x[i],y[5,i])-59*func(x[i-1],y[5,i-1])+37*func(x[i-2],y[5,i-2])-9*func(x[i-3],y[5,i-3]));
+            }
+        
+        }
+        public void runqeKutta() {
+            for (int i = 0; i < n; i++)
+            {
+                k[0, i] = func(x[i],y[4,i]);
+                k[1, i] = func(x[i] + h / 2, y[4, i] + (h / 2) * k[0, i]);
+                k[2, i] = func(x[i] + h / 2, y[4, i] + (h / 2) * k[1, i]);
+                k[3, i] = func(x[i] + h , y[4, i] + h * k[2, i]);
+                y[4,i+1]=y[4,i]+(h/6)*(k[0,i]+2*k[1,i]+2*k[2,i]+k[3,i]);
+            }
+        }
+      
+
+        public double func(double x, double y)
+        {
+        return NUM*x+y;
+        }
+
+        public double fkt(int n) {
+            if (n > 1)
+            {
+                return n * fkt(n - 1);
+            }
+            else return 1;
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        { 
+            label1.BackColor = Color.Green;
+            label1.ForeColor = Color.LightCyan;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.Beige;
+            label1.ForeColor = Color.DarkBlue;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals("") || textBox2.Text.Equals("") || textBox3.Text.Equals("") || textBox4.Text.Equals(""))
+            {
+                MessageBox.Show("Bütün xanaları doldurun!!!");
+            }
+            else
+            {
+                initValues();
+                ashkarEyler();
+                qeyriAshkarEyler();
+                pikar();
+                deqiqHell();
+                runqeKutta();
+                adams();
+                //  MessageBox.Show(factorial(5)+"");
+                for (int i = 0; i < n + 1; i++)
+                {
+                    dataGridView1.Rows.Add(x[i] + "", y[0, i] + "", y[1, i] + "", y[2, i] + "", y[3, i] + "", y[4, i] + "", y[5, i] + "");
+                }
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+
+        }
+
+        private void label7_MouseLeave(object sender, EventArgs e)
+        {
+            label7.BackColor = Color.Beige;
+            label7.ForeColor = Color.DarkBlue;
+        }
+
+        private void label7_MouseMove(object sender, MouseEventArgs e)
+        {
+            label7.BackColor = Color.Red;
+            label7.ForeColor = Color.LightCyan;
+        }
+
+       
+
+        private void label6_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBox5.Visible = !textBox5.Visible;
+            if ((!textBox5.Visible) && (!textBox5.Text.Equals("")))
+            {
+                NUM = int.Parse(textBox5.Text);
+                label6.Text = "N=" + NUM;
+            }
+        }
+
+        
+    }
+}
